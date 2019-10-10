752e792bcc7653f86f336fb68388df31234d870e
Matt Valle <mattvalle28@gmail.com>
2019-10-10 13:23:18 -0400

    java script layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//make pc pick a random number 1-100
+//make 4 img's hold a value 1-10
+//add your score together when one of the 4 imgs are selected 
+//make a win alert when your score is equal to the random pc number 
+//make a lose alert when your score goes over pc random number
+//reset your score when you win or lose
+//reset pc's random number
+//reset image values
