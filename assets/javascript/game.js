e8e328bfab0149e657faf9e322ac91580ef6dbb1
Matt Valle <mattvalle28@gmail.com>
2019-10-11 14:41:55 -0400

    more javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
 //make pc pick a random number 1-100
+var pcRandomNum = (Math.floor(Math.random() * 100) + 1);
+    console.log(pcRandomNum)
+
 //make 4 img's hold a value 1-10
+var jewel1 = (Math.floor(Math.random() * 10) + 1);
+    console.log(jewel1)
+
+var jewel2 = (Math.floor(Math.random() * 10) + 1);
+    console.log(jewel2)
+
+var jewel3 = (Math.floor(Math.random() * 10) + 1);
+    console.log(jewel3)
+
+var jewel4 = (Math.floor(Math.random() * 10) + 1);
+    console.log(jewel4)
+
+//make pcRandomNum show up on randomNum div in html
+
+    
+    
+  
+
+
+//window.onload = function(){
+    //pcGuess(pcRandomNum);
+//}
+    
+
+
 //add your score together when one of the 4 imgs are selected 
 //make a win alert when your score is equal to the random pc number 
 //make a lose alert when your score goes over pc random number
